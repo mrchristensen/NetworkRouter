34e14955124f58cf42a12d8cb922f2a85d2de0a9
Matt Christensen <mr.christensen@gmail.com>
2021-02-24 01:17:39 -0700

    Partial implementation (getShortestPath currently is in inf loop)

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -3,6 +3,7 @@
 
 from CS312Graph import *
 import time
+import math
 
 
 class NetworkRoutingSolver:
@@ -12,6 +13,7 @@ class NetworkRoutingSolver:
     def initializeNetwork( self, network ):
         assert( type(network) == CS312Graph )
         self.network = network
+        self.results = {}
 
     def getShortestPath( self, destIndex ):
         self.dest = destIndex
@@ -21,22 +23,121 @@ class NetworkRoutingSolver:
         #       NEED TO USE
         path_edges = []
         total_length = 0
-        node = self.network.nodes[self.source]
-        edges_left = 3
-        while edges_left > 0:
-            edge = node.neighbors[2]
-            path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
-            total_length += edge.length
-            node = edge.dest
-            edges_left -= 1
-        return {'cost':total_length, 'path':path_edges}
+        node = self.network.nodes[self.dest]
+
+        while node is not None:  # Traverse the graph backwards
+            if self.results[node.node_id]["prev"] is None:
+                path_edges = []
+                total_length = math.inf
+                break
+
+            previous_node = self.network.nodes[self.results[node.node_id]['prev']]
+
+            for neighbor in previous_node.neighbors:
+                if neighbor.dest is node:
+                    total_length = total_length + neighbor.length
+                    path_edges.append((neighbor.src.loc, neighbor.dest.loc, '{:.0f}'.format(neighbor.length)))
+
+            node = previous_node
+
+        return {'cost': total_length, 'path':path_edges}
 
     def computeShortestPaths( self, srcIndex, use_heap=False ):
-        self.source = srcIndex
         t1 = time.time()
-        # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
+
+
+        queue = None
+        if(use_heap):
+            queue = HeapPriorityQueue(self.network)
+        else:
+            queue = UnsortedArrayPriorityQueue(self.network)
+
+        queue.update_key(srcIndex, 0)
+
+        # RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
-        t2 = time.time()
-        return (t2-t1)
 
+        for node in self.network.nodes:
+            self.results[node.node_id] = {'dist': math.inf, 'prev': -1}
+
+        self.results[srcIndex]['dist'] = 0
+
+        while queue.is_not_empty():
+            u = queue.delete_min()
+            edges = self.network.nodes[u['id']].neighbors
+            for edge in edges:
+                v = self.results[edge.dest.node_id]
+                if v['dist'] > u['dist'] + edge.length:
+                    v['dist'] = u['dist'] + edge.length
+                    v['prev'] = u['id']
+                    queue.update_key(edge.dest.node_id, v["dist"])
+
+        t2 = time.time()
+        return t2-t1
+
+
+class UnsortedArrayPriorityQueue:
+    def __init__(self, graph):
+        self.num_nodes = len(graph.nodes)
+        self.queue = {}
+
+        for index in range(self.num_nodes):
+            self.queue[graph.nodes[index].node_id] = {'dist': math.inf}
+
+    def delete_min(self):
+        smallest_index = -1
+        smallest_distance = math.inf
+
+        for index, node in self.queue.items():
+            if self.queue[index]['dist'] < smallest_distance:
+                smallest_distance = self.queue[index]['dist']
+                smallest_index = index
+        smallest_node = {'id': smallest_index, 'dist': smallest_distance}
+        if smallest_index is -1:
+            first_node = self.queue.popitem()
+            return {'id': first_node[0], 'dist': first_node[1]['dist']}
+        del self.queue[smallest_index]
+        return smallest_node
+
+    def update_key(self, index, distance):
+        self.queue[index]['dist'] = distance
+
+    def is_not_empty(self):
+        if len(self.queue) > 0:
+            return True
+        else:
+            return False
+
+
+class HeapPriorityQueue():
+    def __init__(self, graph):
+        self.num_nodes = len(graph.nodes)
+        self.queue = {}
+
+        for index in range(self.num_nodes):
+            self.queue[graph.nodes[index].node_id] = {'dist': math.inf}
+
+    def delete_min(self):
+        smallest_index = -1
+        smallest_distance = math.inf
+
+        for index, node in self.queue.items():
+            if self.queue[index]['dist'] < smallest_distance:
+                smallest_distance = self.queue[index]['dist']
+                smallest_index = index
+        smallest_node = {'id': smallest_index, 'dist': smallest_distance}
+        if smallest_index is -1:
+            first_node = self.queue.popitem()
+            return {'id': first_node[0], 'dist': first_node[1]['dist']}
+        del self.queue[smallest_index]
+        return smallest_node
+
+    def update_key(self, index, distance):
+        self.queue[index]['dist'] = distance
+
+    def is_not_empty(self):
+        if len(self.queue) > 0:
+            return True
+        else:
+            return False
